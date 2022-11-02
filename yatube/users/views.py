a359b06aa093c6a0ecb368c00eb629c0bd6d6f26
Кирилл Черненко <Chernenko.kiry@yandex.ru>
2022-11-02 22:41:11 +0300

    1 com

---
+++ yatube/users/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CreationForm
+from django.core.mail import send_mail
+
+send_mail(
+    'Тема письма',
+    'Текст письма.',
+    'from@example.com',
+    ['to@example.com'],
+    fail_silently=False,)
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
